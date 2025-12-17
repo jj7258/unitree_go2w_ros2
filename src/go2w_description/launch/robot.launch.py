922180c268b217ebb0ff1b0844123305470b093a
Joel Georgie Jacob <joelj7258@gmail.com>
2025-12-17 14:57:37 +0530

    feat: Migrate robot description to XACRO format, add Livox MID-360 LiDAR, and update launch configuration.

--- src/go2w_description/launch/robot.launch.py
+++ src/go2w_description/launch/robot.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             'description_file',
-            default_value='go2w_description.urdf',
+            default_value='go2w.urdf.xacro',
             description='URDF/XACRO description file with the robot.',
         )
     )
